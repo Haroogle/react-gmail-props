7fe3dda746397055fe05aa06caa97c8aac297402
Haroun Anwari <harounanwari@hotmail.co.uk>
2022-11-09 17:29:52 +0000

    Emails and email component made

---
+++ src/Toggleread.js
@@ -0,0 +1,10 @@
+function ToggleRead({ targetEmail }) {
+  (targetEmail) => {
+    const updatedEmails = (emails) =>
+      emails.map((email) =>
+        email.id === targetEmail.id ? { ...email, read: !email.read } : email
+      );
+    setEmails(updatedEmails);
+  };
+}
+export default ToggleRead;
